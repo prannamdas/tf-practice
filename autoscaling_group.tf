4dd889b517e705943c2973b860f8f325ee01c90b
prannamdas <131272269+prannamdas@users.noreply.github.com>
2023-05-14 09:20:20 -0400

    Update autoscaling_group.tf

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_configuration" "aws_launch_configuration_raj" {
-    name_prefix = "aws_launch_configuration_raj"
-    instance_type = var.instance_type
-    image_id = var.ami
-    user_data = file("user-data.sh")
+    name_prefix     = "aws_launch_configuration_raj"
+    image_id        = var.ami
+    instance_type   = var.instance_type
+    user_data       = file("user-data.sh")
     security_groups = [aws_security_group.aws_security_group_raj.id]
 
     lifecycle {
@@ -11,16 +11,21 @@ resource "aws_launch_configuration" "aws_launch_configuration_raj" {
 }
 
 resource "aws_autoscaling_group" "aws_autoscaling_group_raj" {
-    name = "aws_autoscaling_group_raj"
+    name                 = "aws_autoscaling_group_raj"
+    min_size             = 1
+    max_size             = 3
+    desired_capacity     = 2
     launch_configuration = aws_launch_configuration.aws_launch_configuration_raj.name
-    min_size = 1
-    max_size = 3
-    desired_capacity = 2
-    health_check_grace_period = 300
-    vpc_zone_identifier = [aws_subnet.aws_zonea_public_subnet_raj.id,aws_subnet.aws_zoneb_public_subnet_raj.id]
+    vpc_zone_identifier  = [aws_subnet.aws_zonea_public_subnet_raj.id,aws_subnet.aws_zoneb_public_subnet_raj.id]
+
+    tag {
+        key                 = "Name"
+        value               = "alpha-auto-scaling-group"
+        propagate_at_launch = true
+    }
 }
 
 resource "aws_autoscaling_attachment" "aws_autoscaling_attachment_raj" {
     autoscaling_group_name = aws_autoscaling_group.aws_autoscaling_group_raj.id
-    lb_target_group_arn = aws_lb_target_group.aws_lb_target_group_raj.arn
+    lb_target_group_arn    = aws_lb_target_group.aws_lb_target_group_raj.arn
 }
